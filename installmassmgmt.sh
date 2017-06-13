f117346000a038624dabd609d311c97963f70955
Julian Thies <julianmthies@gmail.com>
2017-06-13 15:58:35 -0400

    Create installmassmgmt.sh

---
+++ installmassmgmt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# installmassmgmt.sh
+# install the components of the massmgmt suite in the correct locations
+# Last Edited: 6/13/17
+# -----------------------------------------------------------------------------
+# make directories
+mkdir /usr/local/massmgmt
+mkdir /usr/local/massmgmt/bin
+mkdir /usr/local/massmgmt/sbin
+
+# /usr/local/massmgmt/bin ---------------------
+cd /tmp/massmgmt
+cp $file /usr/local/massmgmt/bin
+
+
+# /usr/local/massmgmt/sbin --------------------
+cd /tmp/massmgmt
+cp $file1 /usr/local/massmgmt/sbin
+
+# create alias to be able to run massmgmt from the command line
+ln -s '/usr/local/massmgmt/massmgmt' '/usr/local/bin/'
+exit
+# -----------------------------------------------------------------------------
