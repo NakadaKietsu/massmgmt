42257234194a5a02ea59d931de961758e0fe0ad0
Julian Thies <julianmthies@gmail.com>
2017-06-15 13:33:45 -0400

    Create installmassmgmt.sh

--- installmassmgmt.sh
+++ installmassmgmt.sh
@@ -7,42 +7,55 @@
 # variables
 adminUser="$(dscl . list /Users UniqueID | awk '$2 == 501 {print $1}')"
 logDir="/Users/$adminUser/Library/Logs/massmgmt"
+
+logFile="/Users/$adminUser/Desktop/installLog.txt"
+starDate="$(date)"
+hostName="$(hostname)"
 # -----------------------------------------------------------------------------
 # Script Contents -------------------------------------------------------------
 
+echo "$starDate -- $hostName ----" >> "$logFile"
+echo >> "$logFile"
+
 # make directories
-mkdir /usr/local/massmgmt
-mkdir /usr/local/massmgmt/bin
-mkdir /usr/local/massmgmt/sbin
-mkdir "$logDir"
+echo "Generating directories" >> "$logFile"
+mkdir /usr/local/massmgmt >> "$logFile"
+mkdir /usr/local/massmgmt/bin >> "$logFile"
+mkdir /usr/local/massmgmt/sbin >> "$logFile"
+mkdir "$logDir" >> "$logFile"
+echo "Directories generated" >> "$logFile"
 
 # /usr/local/massmgmt/ ------------------------
-cd /tmp/massmgmt
-cp massmgmt /usr/local/massmgmt/
+echo "Copying main script" >> "$logFile"
+cd /tmp/massmgmt >> "$logFile"
+cp massmgmt /usr/local/massmgmt/ >> "$logFile"
 
 # /usr/local/massmgmt/bin ---------------------
 copyLoc="/usr/local/massmgmt/bin"
-cd /tmp/massmgmt
-cp autoUpdate "$copyLoc"
-cp chrome "$copyLoc"
-cp enableSSH "$copyLoc"
-cp enableARD "$copyLoc"
-cp setHostname "$copyLoc"
-cp setTimezone "$copyLoc"
+echo "Copying scripts to $copyLoc" >> "$logFile"
+cd /tmp/massmgmt >> "$logFile"
+cp autoUpdate "$copyLoc" >> "$logFile"
+cp chrome "$copyLoc" >> "$logFile"
+cp enableSSH "$copyLoc" >> "$logFile"
+cp enableARD "$copyLoc" >> "$logFile"
+cp setHostname "$copyLoc" >> "$logFile"
+cp setTimezone "$copyLoc" >> "$logFile"
 
 # /usr/local/massmgmt/sbin --------------------
 copyLoc="/usr/local/massmgmt/sbin"
-cd /tmp/massmgmt
+echo "Copying scripts to $copyLoc" >> "$logFile"
+cd /tmp/massmgmt >> "$logFile"
 #cp $file1 "$copyLoc"
 
 # sudo chmod ----------------------------------
 cd /usr/local/massmgmt/bin
-sudo chmod +x autoUpdate
-sudo chmod +x chrome
-sudo chmod +x enableSSH
-sudo chmod +x enableARD
-sudo chmod +x setHostname
-sudo chmod +x setTimezone
+echo "CHMOD scripts" >> "$logFile"
+sudo chmod +x autoUpdate >> "$logFile"
+sudo chmod +x chrome >> "$logFile"
+sudo chmod +x enableSSH >> "$logFile"
+sudo chmod +x enableARD >> "$logFile"
+sudo chmod +x setHostname >> "$logFile"
+sudo chmod +x setTimezone >> "$logFile"
 
 #cd /usr/local/massmgmt/sbin
 
@@ -52,9 +65,10 @@ sudo chmod +x setTimezone
 #cd /usr/local/massmgmt/sbin
 
 # move daemon into place
-sudo cp /tmp/massmgmt/org.massmgmt.plist /Library/LaunchDaemons
+echo "Moving daemon into /Library/LaunchDaemons" >> "$logFile"
+sudo cp /tmp/massmgmt/org.massmgmt.plist /Library/LaunchDaemons >> "$logFile"
 
 # create alias to be able to run massmgmt from the command line
-ln -s '/usr/local/massmgmt/massmgmt' '/usr/local/bin/'
+ln -s '/usr/local/massmgmt/massmgmt' '/usr/local/bin/' >> "$logFile"
 exit
 # -----------------------------------------------------------------------------
